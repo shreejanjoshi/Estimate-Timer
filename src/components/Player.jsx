deeb1b62e0b10d4e46123170bbee395e138aedcb
Shreejan Joshi <shreejanjoshi97@gmail.com>
2023-10-30 09:43:27 +0100

    name will be shown if typed in input field

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enterPlayerName, setEnterPlayerName] = useState(null);
+
+  const handleClick = () => {
+    setEnterPlayerName(playerName.current.value);
+    playerName.current.value = "";
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enterPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerName}
+          type="text"
+        />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
